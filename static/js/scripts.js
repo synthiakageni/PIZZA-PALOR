3e8327fa77c72e7c4c9634eed9e8d91f149bc9ca
Synthia Kageni <synthiakageni@gmail.com>
2021-06-27 14:40:47 +0300

    add to scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,27 +10,25 @@ function Total(size, crust, topping, delivery) {
 
 //A function that executes when customers selects pick option
 function validatePick() {
-  let selectedCrust = parseFloat(document.getElementById("pizza-crust").value);
-  let selectedToppings = parseFloat(document.getElementById("pizza-toppings").value);
-  let selectedSize = parseFloat(document.getElementById("pizza-size").value);
+  let selectedCrust = parseFloat(document.getElementById("crust").value);
+  let selectedToppings = parseFloat(document.getElementById("toppings").value);
+  let selectedSize = parseFloat(document.getElementById("size").value);
   let quantity = document.getElementById("pizzaNumber").value;
   let selectedDelivery = parseFloat(document.getElementById("pick").value);
 
   const pickDelivery = new Total(selectedSize, selectedCrust, selectedToppings, selectedDelivery);
-  // const deliver = new Total(selectedSize + selectedCrust + selectedToppings + delivery);
 
   var cost = pickDelivery.size + pickDelivery.crust + pickDelivery.topping + pickDelivery.delivery
   let totalCost = cost * quantity;
 
   if (selectedCrust == "" || selectedToppings == "" || selectedSize == "" || quantity == "") {
-      $(".alert").show();
+    alert('')
   } else {
-      $(".order-form").hide();
       $(".summary").show();
-      document.getElementById("itemQuantity").innerHTML = quantity;
-      document.getElementById("itemSize").innerHTML = selectedSize;
-      document.getElementById("itemCrust").innerHTML = selectedCrust;
-      document.getElementById("itemTopping").innerHTML = selectedToppings;
+      document.getElementById("pizzaQuantity").innerHTML = quantity;
+      document.getElementById("pizzaSize").innerHTML = selectedSize;
+      document.getElementById("pizzaCrust").innerHTML = selectedCrust;
+      document.getElementById("pizzaTopping").innerHTML = selectedToppings;
       document.getElementById("totalValue").innerHTML = totalCost;
 
   }
@@ -39,9 +37,9 @@ function validatePick() {
 
 //A function that executes when customers selects delivery option
 function validateDeliver() {
-  let selectedCrust = parseFloat(document.getElementById("pizza-crust").value);
-  let selectedToppings = parseFloat(document.getElementById("pizza-toppings").value);
-  let selectedSize = parseFloat(document.getElementById("pizza-size").value);
+  let selectedCrust = parseFloat(document.getElementById("crust").value);
+  let selectedToppings = parseFloat(document.getElementById("toppings").value);
+  let selectedSize = parseFloat(document.getElementById("size").value);
   let selectedDelivery = parseFloat(document.getElementById("deliver").value);
   let quantity = document.getElementById("pizzaNumber").value;
 
@@ -50,62 +48,17 @@ function validateDeliver() {
   var cost = deliver.size + deliver.crust + deliver.topping + deliver.delivery
   let totalCost = cost * quantity;
   if (selectedCrust == "" || selectedToppings == "" || selectedSize == "" || quantity == "") {
-      $(".alert").show();
+      alert('enter your location')
   } else {
-      $(".alertDeliver").show();
-      if (charge) {
-          $(".order-form").hide();
-          $(".summary").show();
-      } else {
-          return false;
-      }
-
-  }
-  if (charge) {
-      $("#itemQuantity").text(quantity);
-      $("#itemSize").text(selectedSize);
-      $("#itemCrust").text(selectedCrust);
-      $("#itemTopping").text(selectedToppings);
-      $("#totalValue").text(totalCost);
+      $(".summary").show();
   }
 }
 
-//This code passes data to receipt table
-function checkOut() {
-  let selectedCrust = parseFloat(document.getElementById("pizza-crust").value);
-  let selectedToppings = parseFloat(document.getElementById("pizza-toppings").value);
-  let selectedSize = parseFloat(document.getElementById("pizza-size").value);
-  let selectedDelivery = parseFloat(document.getElementById("pick").value);
-  let quantity = document.getElementById("pizzaNumber").value;
-  let total = (selectedCrust + selectedToppings + selectedSize + selectedDelivery) * quantity;
-  $("#quantity").text(quantity);
-  $("#size").text(selectedSize);
-  $("#crust").text(selectedCrust);
-  $("#topping").text(selectedToppings);
-  $("#value").text(total);
-}
 
 
 
 //USER INTERFACE LOGIC
-var charge = "There will be an additional charge for transport";
-var noInput = "Kindly provide all fields";
-var confirmTransport = "Are you sure you don't need delivery?";
 
-$(document).ready(function () {
-  $("#bg-order").addClass("order");
-  $(".summary").hide();
-  $(".receipt").hide();
-  $(".order-form").hide();
-  $(".print").hide();
-  $(".alert").hide();
-  $(".alertDeliver").hide();
-
-  //The code below will hide the landing page and display the form to user
-  $("#bg-order").on('click', function () {
-      $("#landing").hide();
-      $(".order-form").show();
-  });
 
   //The code below executes customers inputs for those who are coming the pick their pizza
   $("#pick").on('click', function () {
@@ -113,8 +66,8 @@ $(document).ready(function () {
       if (validatePick() != NaN) {
           validatePick();
       } else {
-          $(".order-form").show();
-          $(".summary").hide();
+
+          $(".summary").shoe();
       }
   });
 
@@ -130,20 +83,3 @@ $(document).ready(function () {
           $(".summary").hide();
       }
   });
-
-  //The code below will show a receipt of what customer has ordered
-
-  $(".checkOut").on('click', function () {
-      $(".order-form").show();
-      $(".receipt").show();
-      $(".summary").hide();
-      $(".print").show();
-      $(".alert").hide();
-      $(".alertDeliver").hide();
-      checkOut();
-  });
-
-  $(".print").on('click', function () {
-      window.print();
-  });
-})
