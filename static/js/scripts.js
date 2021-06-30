ca02ddc3f233999602c4c84c8568396690ac533b
Synthia Kageni <synthiakageni@gmail.com>
2021-06-30 14:36:31 +0300

    add scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,31 +7,31 @@ function Total(size, crust, topping, delivery) {
   this.delivery = delivery;
 }
  
-// function validatePick() {
-//   let selectedCrust = parseFloat(document.getElementById("crust").value);
-//   let selectedToppings = parseFloat(document.getElementById("toppings").value);
-//   let selectedSize = parseFloat(document.getElementById("size").value);
-//   let quantity = document.getElementById("pizzaNumber").value;
-//   let selectedDelivery = parseFloat(document.getElementById("pick").value);
+function validatePick() {
+ let selectedCrust = parseFloat(document.getElementById("crust").value);
+  let selectedToppings = parseFloat(document.getElementById("toppings").value);
+ let selectedSize = parseFloat(document.getElementById("size").value);
+  let quantity = document.getElementById("pizzaNumber").value;
+  let selectedDelivery = parseFloat(document.getElementById("pick").value);
 
-//   const pickDelivery = new Total(selectedSize, selectedCrust, selectedToppings, selectedDelivery);
+  const pickDelivery = new Total(selectedSize, selectedCrust, selectedToppings, selectedDelivery);
 
-//   var cost = pickDelivery.size + pickDelivery.crust + pickDelivery.topping + pickDelivery.delivery
-//   let totalCost = cost * quantity;
+ var cost = pickDelivery.size + pickDelivery.crust + pickDelivery.topping + pickDelivery.delivery
+  let totalCost = cost * quantity;
 
-//   if (selectedCrust == "" || selectedToppings == "" || selectedSize == "" || quantity == "") {
-//     alert('')
-//   } else {
-//       $(".summary").show();
-//       document.getElementById("pizzaQuantity").innerHTML = quantity;
-//       document.getElementById("pizzaSize").innerHTML = selectedSize;
-//       document.getElementById("pizzaCrust").innerHTML = selectedCrust;
-//       document.getElementById("pizzaTopping").innerHTML = selectedToppings;
-//       document.getElementById("totalValue").innerHTML = totalCost;
+  if (selectedCrust == "" || selectedToppings == "" || selectedSize == "" || quantity == "") {  alert('')
+ } 
+ else {
+    $(".summary").show();
+  document.getElementById("pizzaQuantity").innerHTML = quantity;
+    document.getElementById("pizzaSize").innerHTML = selectedSize;
+   document.getElementById("pizzaCrust").innerHTML = selectedCrust;
+     document.getElementById("pizzaTopping").innerHTML = selectedToppings;
+     document.getElementById("totalValue").innerHTML = totalCost;
 
-//   }
+  }
 
-// }
+ }
 
 function validateDeliver() {
   let selectedCrust = parseFloat(document.getElementById("crust").value);
@@ -47,6 +47,12 @@ function validateDeliver() {
 
   let selectedSize = parseFloat(document.getElementById("size").value);
   let selectedDelivery = parseFloat(document.getElementById("deliver").value);
+  switch (selectedDelivery) {
+    case 200:
+      prompt("enter your location..");
+      alert("Your order will be delivered to your location..at a delivery fee of 200")
+      break;
+  }
   let quantity = document.getElementById("pizzaNumber").value;
 
   console.log(selectedCrust);
@@ -55,12 +61,24 @@ function validateDeliver() {
   console.log(selectedDelivery);
   console.log(quantity);
   // location=prompt("enter location.")
+      // alert('your order will be delivered to your location')
 
   const deliver = new Total(selectedSize, selectedCrust, selectedToppings, selectedDelivery);
 
   var cost = selectedSize+selectedCrust+selectedToppings+selectedDelivery
   let totalCost = cost * quantity;
-  alert( 'Total cost is'+ totalCost)
+  // alert( 'Total cost is'+ selectedSize); 
+
+ 
+  
+  $("#pizzaSize").html(selectedSize );
+  $("#pizzaCrust").html(selectedCrust );
+  $("#pizzaTopping").html(selectedToppings )
+  $("#totals").html(totalCost);
+
+
+
+
  
 }
 function reply() {
